cac1a5cb76cf77b579b3e796e49e6c59973fe4cd
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2025-09-11 22:17:10 +0100

    Add namespace to generated thrift types

--- Parquet.FSharp.Thrift/parquet.thrift
+++ Parquet.FSharp.Thrift/parquet.thrift
@@ -22,6 +22,7 @@
  */
 namespace cpp parquet
 namespace java org.apache.parquet.format
+namespace netstd Parquet.FSharp.Thrift
 
 /**
  * Types supported by Parquet.  These types are intended to be used in combination

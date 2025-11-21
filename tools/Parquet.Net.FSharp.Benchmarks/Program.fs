06bd220329deee8f6190d17a9d2d0569ad2ab584
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2025-11-21 21:24:41 +0000

    Simple benchmark

--- tools/Parquet.Net.FSharp.Benchmarks/Program.fs
+++ tools/Parquet.Net.FSharp.Benchmarks/Program.fs
@@ -1,5 +1,63 @@
 ﻿module Parquet.FSharp.Benchmarks.Program
 
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open Microsoft.VSDiagnostics
+open System
+open System.IO
+
+module ParquetNet =
+    let serialize (records: 'Record seq) =
+        use stream = new MemoryStream()
+        Parquet.Serialization.ParquetSerializer.SerializeAsync(records, stream)
+        |> Async.AwaitTask
+        |> Async.Ignore
+        |> Async.RunSynchronously
+
+module ParquetFSharp =
+    let serialize (records: 'Record seq) =
+        use stream = new MemoryStream()
+        Parquet.FSharp.ParquetSerializer.Serialize(records, stream)
+
+module Random =
+    let private Random = Random()
+
+    let bytes count =
+        let bytes = Array.zeroCreate<byte> count
+        Random.NextBytes(bytes)
+        bytes
+
+    let bool () =
+        Random.NextDouble() < 0.5
+
+    let int () =
+        BitConverter.ToInt32(bytes 4)
+
+    let float () =
+        BitConverter.ToDouble(bytes 8)
+
+    let array count (createItem: unit -> 'Item) =
+        Array.init count (fun _ -> createItem ())
+
+[<CPUUsageDiagnoser>]
+//[<DotNetObjectAllocDiagnoser>]
+//[<DotNetObjectAllocJobConfiguration>]
+[<MemoryDiagnoser>]
+type MyBenchmarks() =
+    let rowCount = 1_000_000
+    let records =
+        Array.init rowCount (fun _ ->
+            {| Field2 = Random.int () |})
+
+    //[<Benchmark>]
+    //member this.ParquetNet() =
+    //    ParquetNet.serialize records
+
+    [<Benchmark>]
+    member this.ParquetFSharp() =
+        ParquetFSharp.serialize records
+
 [<EntryPoint>]
 let main _ =
+    let summary = BenchmarkRunner.Run<MyBenchmarks>()
     0

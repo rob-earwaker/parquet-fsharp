0117f0e02a6bed5a29c9f06945a3013e68a1cf4c
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2025-11-27 22:13:01 +0000

    Move schema generation

---
+++ src/Parquet.FSharp/Schema.fs
@@ -0,0 +1,25 @@
+﻿namespace Parquet.FSharp
+
+open Parquet.Schema
+
+module private rec Schema =
+    let private getValueSchema fieldName valueInfo =
+        match valueInfo with
+        | ValueInfo.Atomic atomicInfo ->
+            // TODO: Should we use some of the custom DataField types here, e.g. DecimalDataField?
+            let dataType = atomicInfo.DataDotnetType
+            let isNullable = atomicInfo.IsOptional
+            DataField(fieldName, dataType, isNullable) :> Field
+        | ValueInfo.List listInfo ->
+            let element = getValueSchema ListField.ElementName listInfo.ElementInfo
+            ListField(fieldName, element) :> Field
+        | ValueInfo.Record recordInfo ->
+            let fields = getRecordFields recordInfo
+            StructField(fieldName, fields) :> Field
+
+    let private getRecordFields (recordInfo: RecordInfo) =
+        recordInfo.Fields
+        |> Array.map (fun field -> getValueSchema field.Name field.ValueInfo)
+
+    let ofRecordInfo recordInfo =
+        ParquetSchema(getRecordFields recordInfo)

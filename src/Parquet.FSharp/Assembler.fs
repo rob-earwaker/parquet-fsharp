cd524f874e8a6548cf16acf3afa0168f7f6d8799
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2025-11-29 14:49:08 +0000

    Model as enumerators

--- src/Parquet.FSharp/Assembler.fs
+++ src/Parquet.FSharp/Assembler.fs
@@ -5,81 +5,163 @@ open System
 open System.Collections.Generic
 open System.Linq.Expressions
 
-[<Struct>]
-type AssembledValue<'Value>(exists: bool, repLevel: int, defLevel: int, isDefined: bool, value: 'Value) =
-    member this.Exists = exists
-    member this.RepLevel = repLevel
-    member this.DefLevel = defLevel
-    member this.IsDefined = isDefined
-    member this.Value = value
+type ColumnEnumerator<'DataValue>(maxRepLevel, maxDefLevel, dataColumn: DataColumn) =
+    let repLevelsRequired = maxRepLevel > 0
+    let defLevelsRequired = maxDefLevel > 0
+
+    let valueCount = dataColumn.NumValues
+    let dataValues = dataColumn.DefinedData :?> 'DataValue[]
+    let repLevels = dataColumn.RepetitionLevels
+    let defLevels = dataColumn.DefinitionLevels
+
+    // NULL values are not written to the data array, so keep track of the data
+    // index and the value index separately. For optional values, these will be
+    // different if there are any NULL values in the column.
+    let mutable nextValueIndex = 0
+    let mutable nextDataValueIndex = 0
+
+    let mutable currentRepLevel = 0
+    let mutable currentDefLevel = 0
+    let mutable currentValueDefined = true
+    let mutable currentDataValue = Unchecked.defaultof<'DataValue>
+
+    let updateCurrentRepLevel =
+        if repLevelsRequired
+        then fun () -> currentRepLevel <- repLevels[nextValueIndex]
+        else fun () -> ()
+
+    let moveNextValue =
+        if defLevelsRequired
+        then
+            fun () ->
+                updateCurrentRepLevel ()
+                currentDefLevel <- defLevels[nextValueIndex]
+                if currentDefLevel < maxDefLevel
+                then
+                    currentValueDefined <- false
+                else
+                    currentValueDefined <- true
+                    currentDataValue <- dataValues[nextDataValueIndex]
+                    nextDataValueIndex <- nextDataValueIndex + 1
+                nextValueIndex <- nextValueIndex + 1
+        else
+            // If the definition levels are not required it means they are
+            // always zero, which means the data values are always defined. We
+            // can optimize for this case by only using the next value index and
+            // never setting the level fields.
+            fun () ->
+                currentDataValue <- dataValues[nextValueIndex]
+                nextValueIndex <- nextValueIndex + 1
+
+    member val CurrentRepLevel = currentRepLevel
+    member val CurrentDefLevel = currentDefLevel
+    member val CurrentValueDefined = currentValueDefined
+    member val CurrentDataValue = currentDataValue
+
+    member this.MoveNext() =
+        if nextValueIndex >= valueCount
+        then false
+        else
+            moveNextValue ()
+            true
+
+type CurrentValue<'Value>() =
+    member val RepLevel = 0 with get, set
+    member val DefLevel = 0 with get, set
+    member val Defined = false with get, set
+    member val Value = Unchecked.defaultof<'Value> with get, set
 
 [<AbstractClass>]
-type private ValueAssembler(dotnetType: Type, maxRepLevel, maxDefLevel) =
-    // TODO: Do these need to be members?
-    member this.RepLevelsRequired = maxRepLevel > 0
-    member this.DefLevelsRequired = maxDefLevel > 0
+type private ValueAssembler(dotnetType: Type) =
+    let currentValueVariable =
+        Expression.Variable(
+            typedefof<CurrentValue<_>>.MakeGenericType(dotnetType),
+            "currentValue")
 
-    member this.NextValueExists = Expression.Variable(typeof<bool>, "nextValueExists")
-    member this.NextValueRepLevel = Expression.Variable(typeof<int>, "nextValueRepLevel")
-    member this.NextValueDefLevel = Expression.Variable(typeof<int>, "nextValueDefLevel")
-    member this.NextValueDefined = Expression.Variable(typeof<bool>, "nextValueDefined")
-    member this.NextValue = Expression.Variable(dotnetType, "nextValue")
+    let setCurrentValue (propertyName: string) value =
+        Expression.Assign(
+            Expression.Property(currentValueVariable, propertyName),
+            value)
 
-    abstract member CollectDataColumnVariables : unit -> ParameterExpression seq
-    abstract member CollectChildNextValueVariables : unit -> ParameterExpression seq
+    member this.SetCurrentValueRepLevel(replevel) = setCurrentValue "RepLevel" replevel
+    member this.SetCurrentValueDefLevel(defLevel) = setCurrentValue "DefLevel" defLevel
+    member this.SetCurrentValueDefined(defined) = setCurrentValue "Defined" defined
+    member this.SetCurrentValue(value) = setCurrentValue "Value" value
 
-    member this.CollectNextValueVariables() =
+    abstract member CollectColumnEnumeratorVariables : unit -> ParameterExpression seq
+    abstract member CollectColumnEnumeratorInitializers : unit -> (Expression -> Expression) seq
+
+    abstract member CollectChildCurrentValueVariables : unit -> ParameterExpression seq
+
+    member this.CollectCurrentValueVariables() =
         seq {
-            yield! this.CollectChildNextValueVariables()
-            yield this.NextValueExists
-            if this.RepLevelsRequired then
-                yield this.NextValueRepLevel
-            if this.DefLevelsRequired then
-                yield this.NextValueDefLevel
-                yield this.NextValueDefined
-            yield this.NextValue
+            yield! this.CollectChildCurrentValueVariables()
+            yield currentValueVariable
         }
 
-    abstract member AssembleNextValue : LambdaExpression
+    abstract member MoveNext : LambdaExpression
 
 type private AtomicAssembler(atomicInfo: AtomicInfo, maxRepLevel, maxDefLevel) =
-    inherit ValueAssembler(atomicInfo.DotnetType, maxRepLevel, maxDefLevel)
+    inherit ValueAssembler(atomicInfo.DotnetType)
 
-    let dataColumn = Expression.Variable(typeof<DataColumn>, "dataColumn")
+    let columnEnumeratorType =
+        typedefof<ColumnEnumerator<_>>.MakeGenericType(atomicInfo.DataDotnetType)
 
-    override this.CollectDataColumnVariables() =
-        Seq.singleton dataColumn
+    let columnEnumerator = Expression.Variable(columnEnumeratorType, "valueEnumerator")
 
-    override this.CollectChildNextValueVariables() =
+    let initializeColumnEnumerator (dataColumn: Expression) =
+        Expression.Assign(
+            columnEnumerator,
+            Expression.New(
+                columnEnumeratorType.GetConstructor([| typeof<int>; typeof<int>; typeof<DataColumn> |]),
+                Expression.Constant(maxRepLevel),
+                Expression.Constant(maxDefLevel),
+                dataColumn))
+        :> Expression
+
+    override this.CollectColumnEnumeratorVariables() =
+        Seq.singleton columnEnumerator
+
+    override this.CollectColumnEnumeratorInitializers() =
+        Seq.singleton initializeColumnEnumerator
+
+    override this.CollectChildCurrentValueVariables() =
         Seq.empty
 
-    override this.AssembleNextValue =
+    override this.MoveNext =
         failwith "not implemented!"
 
 type private ListAssembler(listInfo: ListInfo, maxRepLevel, maxDefLevel, elementAssembler: ValueAssembler) =
-    inherit ValueAssembler(listInfo.DotnetType, maxRepLevel, maxDefLevel)
+    inherit ValueAssembler(listInfo.DotnetType)
 
-    override this.CollectDataColumnVariables() =
-        elementAssembler.CollectDataColumnVariables()
+    override this.CollectColumnEnumeratorVariables() =
+        elementAssembler.CollectColumnEnumeratorVariables()
 
-    override this.CollectChildNextValueVariables() =
-        elementAssembler.CollectNextValueVariables()
+    override this.CollectColumnEnumeratorInitializers() =
+        elementAssembler.CollectColumnEnumeratorInitializers()
 
-    override this.AssembleNextValue =
+    override this.CollectChildCurrentValueVariables() =
+        elementAssembler.CollectCurrentValueVariables()
+
+    override this.MoveNext =
         failwith "not implemented!"
 
 type private RecordAssembler(recordInfo: RecordInfo, maxRepLevel, maxDefLevel, fieldAssemblers: ValueAssembler[]) =
-    inherit ValueAssembler(recordInfo.DotnetType, maxRepLevel, maxDefLevel)
+    inherit ValueAssembler(recordInfo.DotnetType)
 
-    override this.CollectDataColumnVariables() =
+    override this.CollectColumnEnumeratorVariables() =
         fieldAssemblers
-        |> Seq.collect _.CollectDataColumnVariables()
+        |> Seq.collect _.CollectColumnEnumeratorVariables()
 
-    override this.CollectChildNextValueVariables() =
+    override this.CollectColumnEnumeratorInitializers() =
         fieldAssemblers
-        |> Seq.collect _.CollectNextValueVariables()
+        |> Seq.collect _.CollectColumnEnumeratorInitializers()
 
-    override this.AssembleNextValue =
+    override this.CollectChildCurrentValueVariables() =
+        fieldAssemblers
+        |> Seq.collect _.CollectCurrentValueVariables()
+
+    override this.MoveNext =
         failwith "not implemented!"
 
 module private rec ValueAssembler =
@@ -149,8 +231,8 @@ type internal Assembler<'Record>() =
 
     let assembleExpr =
         let dataColumns = Expression.Parameter(typeof<DataColumn[]>, "dataColumns")
-        let dataColumnVariables = recordAssembler.CollectDataColumnVariables() |> Array.ofSeq
-        let nextValueVariables = recordAssembler.CollectNextValueVariables() |> Array.ofSeq
+        let columnEnumeratorVariables = recordAssembler.CollectColumnEnumeratorVariables()
+        let currentValueVariables = recordAssembler.CollectCurrentValueVariables() |> Array.ofSeq
         let records = Expression.Variable(typeof<ResizeArray<'Record>>, "records")
         let record = Expression.Variable(typeof<'Record>, "record")
         let loopBreakLabel = Expression.Label("loopBreak")
@@ -158,29 +240,39 @@ type internal Assembler<'Record>() =
         Expression.Lambda<Func<DataColumn[], 'Record[]>>(
             Expression.Block(
                 seq {
-                    yield! dataColumnVariables
-                    yield! nextValueVariables
+                    yield! columnEnumeratorVariables
+                    yield! currentValueVariables
                     yield records
                 },
                 seq {
-                    // let dataColumn1 = dataColumns[0]
-                    // let dataColumn2 = dataColumns[1]
+                    // let columnEnumerator1 = ColumnEnumerator<'DataValue1>(dataColumns[0])
+                    // let columnEnumerator2 = ColumnEnumerator<'DataValue2>(dataColumns[1])
                     // ...
-                    yield! dataColumnVariables
-                        |> Array.mapi (fun index dataColumnVariable ->
-                            Expression.Assign(
-                                dataColumnVariable,
-                                Expression.ArrayAccess(dataColumns, Expression.Constant(index)))
+                    yield! recordAssembler.CollectColumnEnumeratorInitializers()
+                        |> Seq.mapi (fun index initializeColumnEnumerator ->
+                            let dataColumn =
+                                Expression.ArrayAccess(
+                                    dataColumns,
+                                    Expression.Constant(index))
+                            initializeColumnEnumerator dataColumn)
+                    // let currentValue1 = CurrentValue<'Value1>()
+                    // let currentValue2 = CurrentValue<'Value2>()
+                    // ...
+                    yield! currentValueVariables
+                        |> Seq.map (fun currentValue ->
+                            Expression.Assign(currentValue, Expression.New(currentValue.Type))
                             :> Expression)
+                    // let records = ResizeArray<'Record>()
                     yield Expression.Assign(records, Expression.New(records.Type))
                     yield Expression.Loop(
+                        // while True do
                         Expression.Block(
                             [ record ],
+                            // TODO!!!
                             Expression.Break(loopBreakLabel),
+                            // records.Add(record)
                             Expression.Call(records, "Add", [||], record)),
                         loopBreakLabel)
-                    // TODO
-
                     // return records.ToArray()
                     yield Expression.Call(records, "ToArray", [||], [||])
                 }),

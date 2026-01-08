f04f35139053ee62018dee94d926249e70e620dd
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2026-01-08 21:59:53 +0000

    Rename some tests

--- tests/Parquet.FSharp.Tests/Test.Deserialize.TypeCompatibility.fs
+++ tests/Parquet.FSharp.Tests/Test.Deserialize.TypeCompatibility.fs
@@ -21,149 +21,149 @@ let testTypeCompatibility<'Input, 'Output> convert inputRecords =
         Assert.equal inputField1 outputField1
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as int16`` inputRecords =
+let ``int8 deserialized as int16`` inputRecords =
     testTypeCompatibility<int8, int16> int16 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as int32`` inputRecords =
+let ``int8 deserialized as int32`` inputRecords =
     testTypeCompatibility<int8, int32> int32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as int64`` inputRecords =
+let ``int8 deserialized as int64`` inputRecords =
     testTypeCompatibility<int8, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as float32`` inputRecords =
+let ``int8 deserialized as float32`` inputRecords =
     testTypeCompatibility<int8, float32> float32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as float64`` inputRecords =
+let ``int8 deserialized as float64`` inputRecords =
     testTypeCompatibility<int8, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int8 as decimal`` inputRecords =
+let ``int8 deserialized as decimal`` inputRecords =
     testTypeCompatibility<int8, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int16 as int32`` inputRecords =
+let ``int16 deserialized as int32`` inputRecords =
     testTypeCompatibility<int16, int32> int32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int16 as int64`` inputRecords =
+let ``int16 deserialized as int64`` inputRecords =
     testTypeCompatibility<int16, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int16 as float32`` inputRecords =
+let ``int16 deserialized as float32`` inputRecords =
     testTypeCompatibility<int16, float32> float32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int16 as float64`` inputRecords =
+let ``int16 deserialized as float64`` inputRecords =
     testTypeCompatibility<int16, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int16 as decimal`` inputRecords =
+let ``int16 deserialized as decimal`` inputRecords =
     testTypeCompatibility<int16, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int32 as int64`` inputRecords =
+let ``int32 deserialized as int64`` inputRecords =
     testTypeCompatibility<int32, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int32 as float`` inputRecords =
+let ``int32 deserialized as float`` inputRecords =
     testTypeCompatibility<int32, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int32 as decimal`` inputRecords =
+let ``int32 deserialized as decimal`` inputRecords =
     testTypeCompatibility<int32, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``int64 as decimal`` inputRecords =
+let ``int64 deserialized as decimal`` inputRecords =
     testTypeCompatibility<int64, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as int16`` inputRecords =
+let ``uint8 deserialized as int16`` inputRecords =
     testTypeCompatibility<uint8, int16> int16 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as int32`` inputRecords =
+let ``uint8 deserialized as int32`` inputRecords =
     testTypeCompatibility<uint8, int32> int32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as int64`` inputRecords =
+let ``uint8 deserialized as int64`` inputRecords =
     testTypeCompatibility<uint8, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as uint16`` inputRecords =
+let ``uint8 deserialized as uint16`` inputRecords =
     testTypeCompatibility<uint8, uint16> uint16 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as uint32`` inputRecords =
+let ``uint8 deserialized as uint32`` inputRecords =
     testTypeCompatibility<uint8, uint32> uint32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as uint64`` inputRecords =
+let ``uint8 deserialized as uint64`` inputRecords =
     testTypeCompatibility<uint8, uint64> uint64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as float32`` inputRecords =
+let ``uint8 deserialized as float32`` inputRecords =
     testTypeCompatibility<uint8, float32> float32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as float64`` inputRecords =
+let ``uint8 deserialized as float64`` inputRecords =
     testTypeCompatibility<uint8, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint8 as decimal`` inputRecords =
+let ``uint8 deserialized as decimal`` inputRecords =
     testTypeCompatibility<uint8, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as int32`` inputRecords =
+let ``uint16 deserialized as int32`` inputRecords =
     testTypeCompatibility<uint16, int32> int32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as int64`` inputRecords =
+let ``uint16 deserialized as int64`` inputRecords =
     testTypeCompatibility<uint16, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as uint32`` inputRecords =
+let ``uint16 deserialized as uint32`` inputRecords =
     testTypeCompatibility<uint16, uint32> uint32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as uint64`` inputRecords =
+let ``uint16 deserialized as uint64`` inputRecords =
     testTypeCompatibility<uint16, uint64> uint64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as float32`` inputRecords =
+let ``uint16 deserialized as float32`` inputRecords =
     testTypeCompatibility<uint16, float32> float32 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as float64`` inputRecords =
+let ``uint16 deserialized as float64`` inputRecords =
     testTypeCompatibility<uint16, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint16 as decimal`` inputRecords =
+let ``uint16 deserialized as decimal`` inputRecords =
     testTypeCompatibility<uint16, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint32 as int64`` inputRecords =
+let ``uint32 deserialized as int64`` inputRecords =
     testTypeCompatibility<uint32, int64> int64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint32 as uint64`` inputRecords =
+let ``uint32 deserialized as uint64`` inputRecords =
     testTypeCompatibility<uint32, uint64> uint64 inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint32 as float64`` inputRecords =
+let ``uint32 deserialized as float64`` inputRecords =
     testTypeCompatibility<uint32, float> float inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint32 as decimal`` inputRecords =
+let ``uint32 deserialized as decimal`` inputRecords =
     testTypeCompatibility<uint32, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``uint64 as decimal`` inputRecords =
+let ``uint64 deserialized as decimal`` inputRecords =
     testTypeCompatibility<uint64, decimal> decimal inputRecords
 
 [<Property(Skip = "this is not yet supported")>]
-let ``float32 as float64`` inputRecords =
+let ``float32 deserialized as float64`` inputRecords =
     testTypeCompatibility<float32, float> float inputRecords
